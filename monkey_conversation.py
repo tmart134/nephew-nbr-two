b4491934d785bfad5da9bdea8891c4797692edbe
tmart134 <martinltracey@gmail.com>
2025-03-21 20:41:55 -0400

    initial commit

---
+++ monkey_conversation.py
@@ -0,0 +1,47 @@
+import time 
+
+def monkey():
+    print('fat monkey!')
+    time.sleep(3) 
+    print('why are you saying fat monkey!!!!!!!!')
+    time.sleep(3)
+    print('for funnys.')
+    time.sleep(2)
+    print('bro just stop.')
+    time.sleep(2)
+    print('but it is goofy haaa!')
+    time.sleep(4)
+    print('na im just going to leave man. \nman left call...')
+    time.sleep(4)
+    print('alarm alarm!')
+    time.sleep(2)
+    print('big monkey has ben seen in the streets!')
+    time.sleep(3)
+    print('.')
+    time.sleep(1)
+    print('.')
+    time.sleep(1)
+    print('.')
+    time.sleep(2)
+    print('what the heck?!')
+    time.sleep(3)
+    print('we go should help.')
+    time.sleep(3)
+    print('but what if we get hurt.')
+    time.sleep(4)
+    print('ya we shuld proble not go.')
+    time.sleep(2)
+    print('the end.')
+    time.sleep(6)
+
+monkey()
+
+# def monkeyName(name):
+
+#     print('man has joined the call')
+#     print("monkey " + name)
+
+
+# monkeyName('coll man')
+
+# monkeyName('zxcvbnm')
